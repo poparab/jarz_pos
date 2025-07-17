87ff0f7da9d3ef07a313fd0dcf3052c52712a21a
poparab <abdelrahmanmamdouh1996@gmail.com>
2025-07-17 12:42:53 +0000

    chore(cleanup): remove unneeded Python test scripts

--- jarz_pos/jarz_pos/doctype/city/test_city.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2025, Jarz and contributors
-# For license information, please see license.txt
-
-import unittest
-# import frappe
- 
-class TestCity(unittest.TestCase):
-	pass 
--- jarz_pos/jarz_pos/doctype/jarz_bundle/test_jarz_bundle.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2025, Abdelrahman Mamdouh and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class TestJarzBundle(FrappeTestCase):
-	pass
